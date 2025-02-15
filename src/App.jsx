3e136daf8c510aff479e4b83e44d27f24fa8d729
Miguel Serrano <miguelserrano@Mac.lan>
2025-02-15 14:28:22 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+//pages
+import Hero from './components/Hero'
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Hero />
+      </div>
+    </>
+  )
+}
+
+export default App
